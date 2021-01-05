a3c7abf7925b0375193f9eb0a322b11018b724d6
rahul.mourya <rahul.mourya@rishabhsoft.com>
2021-01-05 22:18:51 +0530

    [Add] netlify fix

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const notFound = () => {
-    return (
-        <h1 className="display-1 p-5 m-5 card"> <span>404</span> Page not found</h1>
-    );
+class notFound extends Component {
+    render() {
+        return (
+            <h1 className="display-1 p-5 m-5 card"><span>404</span> Page not found</h1>
+        );
+    }
 }
 
 export default notFound;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Todo from './Todo/Todo';
-import NotFound from './NotFound/NotFound';
+// import NotFound from './NotFound/NotFound';
 import CurrencyConvertor from './CurrencyConvertor/CurrencyConvertor';
 import reportWebVitals from './reportWebVitals';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
@@ -11,18 +11,15 @@ import { Route, BrowserRouter as Router } from 'react-router-dom'
 const routing = (
   <Router>
     <div>
-      <Route path="#/app" component={App} />
-      <Route path="#/todo" component={Todo} />
-      <Route path="#/currency-convertor" component={CurrencyConvertor} />
-      <Route path="" component={NotFound} />
+      <Route path="/app" component={App} />
+      <Route path="/todo" component={Todo} />
+      <Route path="/currency-convertor" component={CurrencyConvertor} />
+      {/* <Route path="*" component={NotFound} /> */}
     </div>
   </Router>
 )
 
-ReactDOM.render(
-  routing,
-  document.getElementById('root')
-);
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
