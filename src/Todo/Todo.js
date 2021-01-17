5e66a987a1f8c960e299b9a7c8966764a54b4c5d
rahul.mourya <rahul.mourya@rishabhsoft.com>
2021-01-17 21:33:19 +0530

    [Feat] Edit | Todo

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -3,11 +3,13 @@ import './Todo.css';
 
 class todo extends Component {
     defaultInputClass = ['form-control mb-2', 'border-danger shake-input form-control mb-2'];
+    defaultTodoInputClasses = ['p-2 mb-2 col-12 border row m-0 shadow-sm rounded', 'p-2 mb-2 col-12 border row m-0 shadow-sm rounded bg-yellowish'];
     _input = React.createRef();
     state = ({
         inputValue: [],
         currentValue: '',
-        inputClasses: this.defaultInputClass[0]
+        inputClasses: this.defaultInputClass[0],
+        valueUpdateAtIndex: undefined,
     });
 
 
@@ -17,18 +19,19 @@ class todo extends Component {
             <div className="col-md-8 col-xl-6  col-sm-12 m-auto p-4">
                 <h1 className="mb-3 display-5"> ⚛️ Todo Application</h1>
                 <input type="text" className={this.state.inputClasses} placeholder="New Task" onChange={event => this.inputChangeListener(event)} required={true} onKeyPress={this.enterKeyHandler.bind(this)} autoFocus={true} value={this.state.currentValue} ref={c => (this._input = c)} />
-                <button className="btn btn-sm btn-warning" onClick={this.addTaskHandler.bind(this)} type="submit">Add Task</button>
+                <button className="btn btn-sm btn-warning" onClick={this.addTaskHandler.bind(this)} type="submit">{this.state.valueUpdateAtIndex !== undefined ? 'Update Task' : 'Add Task'}</button>
                 <hr />
 
                 {
                     this.state.inputValue.map((item, index) =>
-                        <div className="p-2 mb-2 col-12 border row m-0 shadow-sm rounded" key={item.idx}>
-                            <div className="col-11 p-0 text-break ">
-                                <input type="checkbox" className="form-check-input m-1 center-width" id={item.idx} onClick={this.markAsCompleted.bind(this, index)} checked={item.isCompleted} />
-                                <label className={item.isCompleted ? 'completed_task form-check-label w-94' : 'form-check-label w-94'} for={item.idx}>{item.value}</label>
+                        <div className={item.classes} key={item.idx}>
+                            <div className="col-10 p-0 text-break ">
+                                <input type="checkbox" className="form-check-input m-1 center-width" id={item.idx} onClick={this.markAsCompleted.bind(this, index)} onChange={e => {}} checked={item.isCompleted} />
+                                <label className={item.isCompleted ? 'completed_task form-check-label w-94' : 'form-check-label w-94'} htmlFor={item.idx}>{item.value}</label>
                             </div>
-                            <div className="col-1">
-                                <i className="bi bi-trash text-danger c-pointer" onClick={this.deleteTaskHandler.bind(this, index)}></i>
+                            <div className="col-2 m-0 p-0 text-center">
+                                <i className={this.state.valueUpdateAtIndex !== undefined ? "bi bi-pencil-fill text-info c-pointer no-pointer-event" : "bi bi-pencil-fill text-info c-pointer "} onClick={this.editTaskHandler.bind(this, index)} ></i>
+                                <i className="bi bi-trash text-danger c-pointer p-4" onClick={this.deleteTaskHandler.bind(this, index)}></i>
                             </div>
                         </div>
                     )
@@ -45,12 +48,22 @@ class todo extends Component {
             this.setState({ inputClasses: this.defaultInputClass[0] });
         }, 1500);
 
-        if (!this.valueExist(this.state.currentValue) && this.state.currentValue) {
+        if (this.state.valueUpdateAtIndex !== undefined) {
+
             const originalState = this.state.inputValue;
-            const newStateItem = [{ value: this.state.currentValue, isCompleted: false, idx: this.state.inputValue ? this.state.inputValue.length + 1 : 0 }];
-            Array.prototype.push.apply(originalState, newStateItem);
-            const data = [...originalState.filter(x => x.isCompleted), ...originalState.filter(x => !x.isCompleted)];
-            this.setState({ inputValue: data });
+            originalState[this.state.valueUpdateAtIndex]['value'] = this.state.currentValue;
+            originalState[this.state.valueUpdateAtIndex]['classes'] = this.defaultTodoInputClasses[0];
+            console.log(this.state);
+            // originalState.splice(this.state.valueUpdateAtIndex, 1, updatedValue);
+            this.setState({ valueUpdateAtIndex: undefined,  })
+        } else {
+            if (!this.valueExist(this.state.currentValue) && this.state.currentValue) {
+                const originalState = this.state.inputValue;
+                const newStateItem = [{ value: this.state.currentValue, isCompleted: false, idx: this.state.inputValue ? this.state.inputValue.length + 1 : 0, classes: this.defaultTodoInputClasses[0] }];
+                Array.prototype.push.apply(originalState, newStateItem);
+                const data = [...originalState.filter(x => x.isCompleted), ...originalState.filter(x => !x.isCompleted)];
+                this.setState({ inputValue: data });
+            }
         }
         console.log(this.state, this.state.inputValue);
         this.setState({ currentValue: '' });
@@ -71,7 +84,19 @@ class todo extends Component {
     deleteTaskHandler(idx) {
         const updatedState = this.state.inputValue;
         updatedState.splice(idx, 1);
-        this.setState({ inputValue: updatedState });
+        const updateFieldIfInEditMode = {};
+        if (this.state.valueUpdateAtIndex !== undefined) {
+            updateFieldIfInEditMode.valueUpdateAtIndex = undefined;
+            updateFieldIfInEditMode.currentValue = '';
+        }
+        this.setState({ inputValue: updatedState, ...updateFieldIfInEditMode });
+    }
+
+    editTaskHandler(idx) {
+        const todoList = this.state.inputValue;
+        todoList[idx]['classes'] = this.defaultTodoInputClasses[1];
+        const valueToEdit = this.state.inputValue[idx]['value'];
+        this.setState({ valueUpdateAtIndex: idx, currentValue: valueToEdit || '', inputValue: todoList});
     }
 
     inputChangeListener(event) {
