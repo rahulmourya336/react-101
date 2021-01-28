a2c30c5fd3edf0888033f2c17eaa69e0d3c128d7
rahul.mourya <rahul.mourya@rishabhsoft.com>
2021-01-28 21:55:44 +0530

    [Update] Todo component

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -3,7 +3,7 @@ import './Todo.css';
 
 class todo extends Component {
     defaultInputClass = ['form-control mb-2', 'border-danger shake-input form-control mb-2'];
-    defaultTodoInputClasses = ['p-2 mb-2 col-12 border row m-0 shadow-sm rounded', 'p-2 mb-2 col-12 border row m-0 shadow-sm rounded bg-yellowish'];
+    defaultTodoInputClasses = ['p-2 mb-2 row m-0 shadow-sm rounded card-width card m-2 border-bottom', 'card-width card m-2 p-2 mb-2 row m-0 shadow-sm rounded bg-yellowish border-bottom'];
     _input = React.createRef();
     state = ({
         inputValue: [],
@@ -21,23 +21,25 @@ class todo extends Component {
                 <input type="text" className={this.state.inputClasses} placeholder="New Task" onChange={event => this.inputChangeListener(event)} required={true} onKeyPress={this.enterKeyHandler.bind(this)} autoFocus={true} value={this.state.currentValue} ref={c => (this._input = c)} />
                 <button className={this.state.valueUpdateAtIndex !== undefined ? "btn btn-sm bg-warning w-100" : "btn btn-sm bg-purple w-100 text-light"} onClick={this.addTaskHandler.bind(this)} type="submit">{this.state.valueUpdateAtIndex !== undefined ? 'Update Task' : 'Add Task'}</button>
                 <hr />
-
-                {
-                    this.state.inputValue.map((item, index) =>
-                        <div className={item.classes} key={item.idx}>
-                            <div className="col-10 p-0 text-break ">
-                                <input type="checkbox" className="form-check-input m-1 center-width" id={item.idx} onClick={this.markAsCompleted.bind(this, index)} onChange={e => { }} checked={item.isCompleted} />
-                                <label className={item.isCompleted ? 'completed_task form-check-label w-94' : 'form-check-label w-94'} htmlFor={item.idx}>{item.value}</label>
+                <div className="todo-container">
+                    {
+                        this.state.inputValue.map((item, index) =>
+                            <div className={item.classes} key={item.idx}>
+                                <div className="col-10 p-0 text-break ">
+                                    <input type="checkbox" className="form-check-input m-1 center-width" id={item.idx} onClick={this.markAsCompleted.bind(this, index)} onChange={e => { }} checked={item.isCompleted} />
+                                    <label className={item.isCompleted ? 'completed_task form-check-label w-94' : 'form-check-label w-94'} htmlFor={item.idx}>{item.value}</label>
+                                </div>
+                                <div className="col-1 m-0 p-0 text-center edit-icon">
+                                    <i className={this.state.valueUpdateAtIndex !== undefined ? "bi bi-pencil-fill text-info c-pointer no-pointer-event text-muted" : "bi bi-pencil-fill text-info c-pointer text-muted"} onClick={this.editTaskHandler.bind(this, index)} ></i>
+                                </div>
+                                <div className="col-1 m-0 p-0 text-center remove-icon">
+                                    <i className="bi bi-trash text-danger c-pointer ml-4" onClick={this.deleteTaskHandler.bind(this, index)}></i>
+                                </div>
                             </div>
-                            <div className="col-1 m-0 p-0 text-center">
-                                <i className={this.state.valueUpdateAtIndex !== undefined ? "bi bi-pencil-fill text-info c-pointer no-pointer-event text-muted" : "bi bi-pencil-fill text-info c-pointer text-muted"} onClick={this.editTaskHandler.bind(this, index)} ></i>
-                            </div>
-                            <div className="col-1 m-0 p-0 text-center">
-                                <i className="bi bi-trash text-danger c-pointer ml-4" onClick={this.deleteTaskHandler.bind(this, index)}></i>
-                            </div>
-                        </div>
-                    )
-                }
+                        )
+                    }
+                </div>
+                {/* End of Todo Container */}
             </div>
         );
     }
