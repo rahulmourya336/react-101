69e7c3d3be15a2f693977cf88111c73f11d8de92
rahul.mourya <rahul.mourya@rishabhsoft.com>
2021-02-16 12:23:50 +0530

    [Add] QG | Loader

--- src/QuotesGenerator/QuotesGenerator.js
+++ src/QuotesGenerator/QuotesGenerator.js
@@ -10,33 +10,44 @@ class QuotesGenerator extends Component {
         quote: {
             title: '',
             author: ''
-        }
+        },
+        loading: false
     };
     render() {
+        const Loader = () => <div className="loader">
+            <div className="loading position-absolute top-50 start-50">
+                <span className="position-absolute p-5">Downloading...</span>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
         return (
             <>
+                { this.state.loading ? <Loader /> : null}
                 <div className="container p-4">
+
                     <div className="row">
                         <div className="col rs-fix">
-                            <h1>Quotes Generator</h1>
+                            <h1>📜 Quotes Generator</h1>
                         </div>
                         <div className="col text-end">
                             <button className="btn btn-outline-primary text-right align-self-center" onClick={this.init.bind(this)}>Motivate Me</button>
                         </div>
                     </div>
-                    <div className="position-relative mt-4 shadow-lg">
+                    <div className="position-relative mt-4">
                         <div id="quoteImage">
                             <img src={this.state.imageURL} alt="quotes image" aria-hidden className="img-thumbnail" />
-                            <blockquote className="position-absolute top-50-local" contentEditable="true">{this.state.quote.text} - <cite>{this.state.quote.author || 'Anonymus'}</cite></blockquote>
+                            <blockquote className="position-absolute top-50-local" contentEditable="true" suppressContentEditableWarning={true}>{this.state.quote.text} - <cite>{this.state.quote.author || 'Anonymus'}</cite></blockquote>
                         </div>
-                        <button className="btn btn-warning text-light position-absolute bottom-0 end-0 download-btn btn-sm" onClick={this.downloadAsImage.bind(this, 'quoteImage')}><span className="material-icons">
+                        <button className="btn btn-primary text-light position-absolute bottom-0 end-0 download-btn btn-sm" onClick={this.downloadAsImage.bind(this, 'quoteImage')}><span className="material-icons">
                             file_download
                         </span></button>
                     </div>
-                    <div className="mt-2"><span className="text-black-50 alert-dark p-1 small"><span><span className="material-icons align-bottom mr-2">
+                    <div className="mt-2"><span className="text-black-50 alert-dark p-1 small"><span className="material-icons align-bottom mr-2">
                         info
-                        </span>Tap on quote to edit</span></span></div>
-                    <div className="bg-yellowish bottom-0 footer m-1 p-2 position-absolute small start-50 text-black-50 text-center translate-middle-x footer">Built with Keyboard by <a href="https://twitter.com/rahucrux" className="text-decoration-none">Rahul Mourya</a></div>
+                        </span><span className="m-2">Tap on quote to edit</span></span></div>
+                    <div className="bg-yellowish bottom-0 footer m-1 p-2 position-absolute small start-50 text-black-50 text-center translate-middle-x footer">Built with ⌨️ by <a href="https://twitter.com/rahucrux" className="text-decoration-none">Rahul Mourya</a></div>
                 </div>
             </>
         );
@@ -52,6 +63,8 @@ class QuotesGenerator extends Component {
     }
 
     downloadAsImage(div) {
+        const self = this;
+        self.setState({ loading: true });
         html2canvas(document.getElementById(div), { useCORS: true, letterRendering: 1, allowTaint: true }).then(function (canvas) {
             document.body.appendChild(canvas);
             var a = document.createElement('a');
@@ -59,6 +72,7 @@ class QuotesGenerator extends Component {
             a.download = `Quote${Date.now()}.jpg`;
             a.click();
             document.body.removeChild(canvas);
+            self.setState({ loading: false });
         });
     }
 
