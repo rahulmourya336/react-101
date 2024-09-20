9b3a9fa20a69758fd08400a0c628e56a625b303b
Rahul Mourya <rahul@heals.asia>
2024-09-20 11:45:12 +0530

    add: code cleanup

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Todo from './Todo/Todo';
 import Dashboard from './Dashboard/Dashboard';
-// import NotFound from './NotFound/NotFound';
 import NumberGuessing from './NumberGuessing/NumberGuessing';
-import CurrencyConvertor from './CurrencyConvertor/CurrencyConvertor';
-import CheckoutPage from './CheckoutPage/CheckoutPage';
 import QuotesGenerator from './QuotesGenerator/QuotesGenerator';
 
 import reportWebVitals from './reportWebVitals';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import notFound from './NotFound/NotFound';
 
 const routing = (
   <Router>
     <Switch>
       <Route exact path="/" component={Dashboard} />
-      <Route path="/app" component={App} />
       <Route path="/todo" component={Todo} />
-      <Route path="/currency-convertor" component={CurrencyConvertor} />
       <Route path="/number-guessing" component={NumberGuessing} />
-      <Route path="/checkout-page" component={CheckoutPage} />
       <Route path="/quotes-generator" component={QuotesGenerator} />
-      {/* <Route path="*" component={NotFound} /> */}
+      <Route path='/404' component={notFound} />
+      <Redirect from='*' to='/404' />
     </Switch>
   </Router>
 )
 
 ReactDOM.render(routing, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
