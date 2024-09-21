541e0416383a82cef0353b8fefe60ea7fbf9fafa
Rahul Mourya <rahul@heals.asia>
2024-09-21 13:41:25 +0530

    add:projects

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import notFound from './NotFound/NotFound';
 
+const PROJECT_LINKS = {
+  CONTACT_FORM: "https://fm-contact-form-jade.vercel.app/",
+  ADVICE_GENERATOR: "https://fm-advice-generator-mocha.vercel.app/",
+}
+
 const routing = (
   <Router>
     <Switch>
       <Route exact path="/" component={Dashboard} />
+      <Route path="/fm-contact-form" component={() => {
+        window.location.replace(PROJECT_LINKS.CONTACT_FORM);
+        return null;
+      }} />
+      <Route path="/fm-advice-generator" component={() => {
+        window.location.replace(PROJECT_LINKS.ADVICE_GENERATOR);
+        return null;
+      }} />
       <Route path="/todo" component={Todo} />
       <Route path="/number-guessing" component={NumberGuessing} />
       <Route path="/quotes-generator" component={QuotesGenerator} />
