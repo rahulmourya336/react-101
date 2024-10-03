183d14fb9c0d923a8c44e5956ad0a24fdbc246b0
Rahul Mourya <rahul@heals.asia>
2024-10-03 15:58:24 +0530

    fix: vercel build issue

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
-        build: {
-            outDir: 'build',
-        },
         base: '/',
         plugins: [react()],
     };
